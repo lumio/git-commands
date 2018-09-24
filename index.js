735bc2e9bd50a340e1003e46bec3f9f54817f1b1
Benjamin Falk <benjamin.falk@cyanjoe.com>
2018-09-24 20:18:06 +0200

    GITC-1 Test out new libraries

--- index.js
+++ index.js
@@ -1,54 +1,21 @@
-const blessed = require( 'blessed' );
+const chalk = require( 'chalk' );
+const hasUnicode = require( 'has-unicode' );
+const stringWidth = require( 'string-width' );
+const windowSize = require( 'window-size' );
+const sliceAnsi = require( 'slice-ansi' );
 
-const screen = blessed.screen( {
-  smartCSR: true,
-  autoPadding: true,
-} );
+if ( hasUnicode() ) {
+  console.log( 'Supports unicode... probably' );
+}
+else {
+  console.log( 'Does not supports unicode... probably' );
+}
 
-screen.title = 'my window title';
-screen.height = 3;
-const box = blessed.box( {
-  top: 'center',
-  left: 'center',
-  width: '80%',
-  height: '20%',
-  content: 'Hello {bold}world{/bold}!',
-  tags: true,
-  border: {
-    type: 'line',
-  },
-  style: {
-    fg: 'white',
-    bg: 'gray',
-    border: {
-      fg: '#f0f0f0',
-    },
-    hover: {
-      bg: 'green',
-    },
-  },
-} );
+console.log( 'Size' );
+console.log( windowSize.get() );
 
-// If our box is clicked, change the content.
-box.on('click', function(data) {
-  box.setContent('{center}Some different {red-fg}content{/red-fg}.{/center}');
-  screen.render();
-});
+console.log( 'String width', stringWidth( '😇 test' ) );
 
-// If box is focused, handle `enter`/`return` and give us some more content.
-box.key('enter', function(ch, key) {
-  box.setContent('{right}Even different {black-fg}content{/black-fg}.{/right}\n');
-  box.setLine(1, 'bar');
-  box.insertLine(1, 'foo');
-  screen.render();
-});
-
-screen.append( box );
-
-// Quit on Escape, q, or Control-C.
-screen.key( [ 'escape', 'q', 'C-c' ], function( ch, key ) {
-  return process.exit( 0 );
-} );
-
-// box.focus();
-screen.render();
+const input =
+  chalk.blue( 'Hello world' ) + chalk.red( 'testing' );
+console.log( sliceAnsi( input, 2, 12 ) );
