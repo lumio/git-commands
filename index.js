b0ba45984eab5ccbebac17eb9617d2623da301db
Benjamin Falk <benjamin.falk@cyanjoe.com>
2018-09-26 22:33:09 +0200

    GITC-1 Basic input elements

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ const t = new Terminal();
   const { cursorPos } = t.state;
   console.log( cursorPos );
 
+  t.subscribe( 'data', ( { char, key } ) => {
+    if ( key.name === 'return' ) {
+      console.log();
+    }
+    else if ( key.name === 'backspace' ) {
+      t.moveCursor( -1, 0 );
+      t.print( ' ' );
+      t.moveCursor( -1, 0 );
+    }
+    else {
+      t.print( char );
+    }
+  } );
   t.render( [
     chalk.cyan( 'GITC-1' ) + ' A rather long commit message that exceeds th'
     + chalk.bgRed( 'e 50 char limit' )
