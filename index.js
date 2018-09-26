015265cdca19494248dc034feb038c8d4c22a4c4
Benjamin Falk <benjamin.falk@cyanjoe.com>
2018-09-26 11:42:53 +0200

    GITC-1 Render a test string

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
+const chalk = require( 'chalk' );
 const Terminal = require( './lib/terminal' );
 
 const t = new Terminal();
 
 t.start();
+t.render( [
+  chalk.cyan( 'GOR-1' ) + ' A rather long commit message that exceeds th'
+  + chalk.bgRed( 'e 50 char limit' )
+] );
